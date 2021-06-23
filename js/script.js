c816e408cd02e908d4573bc8eb10d1de56125b1c
Roberto <robertoanfuso98@gmail.com>
2021-06-23 15:49:59 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const app= new Vue(
+    {
+        el: "#app" ,
+        data:{
+            todo:[
+                "sistemare mark III",
+                "addestrare spider-man",
+                "comprare fiori per Pepper",
+                
+            ]
+        }
+    }
+);

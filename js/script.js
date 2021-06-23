81f08df45f87609110c3210c7b716a2a84e21c7b
Roberto <robertoanfuso98@gmail.com>
2021-06-23 16:33:40 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@ const app= new Vue(
     {
         el: "#app" ,
         data:{
+            newtodo:"",
             todo:[
                 "sistemare mark III",
                 "addestrare spider-man",
-                "comprare fiori per Pepper",
-                
+                "comprare pupazzo coniglio per Pepper",
+                "giocare con Morgan",
+
             ]
-        }
+        },
+        methods:{
+            addtodo(){
+                this.todo.push(this.newtodo);
+                this.newtodo="";
+            },
+            removetodo(index){
+                this.todo.splice(index,1);
+            }
+        },
     }
 );
